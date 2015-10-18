9e1101014253f0ce67f14a6438a0014956a462df
GuilinDev <GuilinDev@gmail.com>
2015-10-18 17:18:45 -0500

    Some questions

---
+++ Algorithms/src/com/guilin/others/AllTest.java
@@ -0,0 +1,14 @@
+package com.guilin.others;
+
+public class AllTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AllTest.testMethod();
+
+	}
+	private static void testMethod(){
+		System.out.println();
+	}
+
+}
